e7c7dd1fa27e21109043a8e17a725dec645b65cb
Benjamin Sampson <benhsampson@protonmail.com>
2021-12-30 23:36:13 +1100

    Feat: Sending emails

--- packages/api/src/utils/sendEmail.ts
+++ packages/api/src/utils/sendEmail.ts
@@ -1,20 +1,32 @@
 import nodemailer from 'nodemailer';
+import fs from 'fs';
+import path from 'path';
 
-import { EMAIL_FROM } from '../constants';
+type MailKey = {
+  client_id: string;
+  private_key: string;
+};
 
 export async function sendEmail(subject: string, to: string, html: string) {
+  const mailKey = JSON.parse(
+    fs.readFileSync(path.join(__dirname, '../../mailKey.json')).toString()
+  ) as MailKey;
+  console.log(process.env.NODEMAILER_USERNAME, mailKey);
+
   const transporter = nodemailer.createTransport({
-    host: 'smtp.ethereal.email',
-    port: 587,
-    secure: false,
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
     auth: {
+      type: 'OAuth2',
       user: process.env.NODEMAILER_USERNAME,
-      pass: process.env.NODEMAILER_PASSWORD,
+      serviceClient: mailKey.client_id,
+      privateKey: mailKey.private_key,
     },
   });
 
   const info = await transporter.sendMail({
-    from: EMAIL_FROM,
+    from: process.env.NODEMAILER_USERNAME,
     to,
     subject,
     html,
